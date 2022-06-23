b11b39b8fee05cbc1158c626596575ba83e94db0
ThomasKnust <toknust@gmail.com>
2022-06-22 22:19:29 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/knustthomas/cursomc/resources/exeption/ValidationError.java
@@ -0,0 +1,25 @@
+package com.knustthomas.cursomc.resources.exeption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(String msg, Integer status, Long timeStamp) {
+		super(msg, status, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	
+
+}
